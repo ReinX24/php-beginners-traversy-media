af3959bcc4725ec335079f3bb982a8ba6d83f902
Rein <reinX244@gmail.com>
2024-04-23 21:24:41 +0800

    Added code up until uploading and showing uploaded images.

--- 14_product_crud/create.php
+++ 14_product_crud/create.php
@@ -6,7 +6,11 @@ $pdo = new PDO(
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $_SERVER["REQUEST_METHOD"] . "<br>";
+$errors = [];
+
+$title = "";
+$price = "";
+$description = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $title = $_POST["title"];
@@ -14,21 +18,66 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $price = $_POST["price"];
     $date = date("Y-m-d H:i:s");
 
-    $insert_query =
-        "INSERT INTO 
-        products (title, image, description, price, create_date)
-    VALUE 
-        (:title, :image, :description, :price, :date)";
+    if (!$title) {
+        $errors[] = "Product title is required";
+    }
 
-    $statement = $pdo->prepare($insert_query);
+    if (!$price) {
+        $errors[] = "Product price is required";
+    }
 
-    $statement->bindValue(":title", $title);
-    $statement->bindValue(":image", "");
-    $statement->bindValue(":description", $description);
-    $statement->bindValue(":price", $price);
-    $statement->bindValue(":date", $date);
+    if (!is_dir("images")) {
+        mkdir("images");
+    }
 
-    $statement->execute();
+    if (empty($errors)) {
+        // Uploading an image
+        $image = $_FILES["image"] ?? null;
+        $imagePath = "";
+
+        // Checks if the image exists and if it has a tmp_name (file detected)
+        if ($image && $image["tmp_name"]) {
+
+            // Creating a unique image path
+            $imagePath = "images/" . randomString(8) . "/" . $image["name"];
+
+            // Creating a unique folder for the image
+            mkdir(dirname($imagePath));
+
+            move_uploaded_file($image["tmp_name"], $imagePath);
+        }
+
+        $insert_query =
+            "INSERT INTO 
+                products (title, image, description, price, create_date)
+            VALUE 
+                (:title, :image, :description, :price, :date)";
+
+        $statement = $pdo->prepare($insert_query);
+
+        $statement->bindValue(":title", $title);
+        $statement->bindValue(":image", $imagePath);
+        $statement->bindValue(":description", $description);
+        $statement->bindValue(":price", $price);
+        $statement->bindValue(":date", $date);
+
+        $statement->execute();
+
+        // Redirect the user to the index page
+        header("Location: index.php");
+    }
+}
+
+function randomString($strLen)
+{
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $randomstr = "";
+    for ($i = 0; $i < $strLen; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomstr .= $characters[$index];
+    }
+
+    return $randomstr;
 }
 ?>
 
@@ -50,7 +99,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <body>
     <h1>Create new Product</h1>
 
-    <form method="POST">
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?= $error; ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <form method="POST" enctype="multipart/form-data">
         <div class="mb-3">
             <label class="form-label">Product Image</label>
             <br>
@@ -58,15 +115,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         </div>
         <div class="mb-3">
             <label class="form-label">Product Title</label>
-            <input type="text" name="title" class="form-control">
+            <input type="text" name="title" class="form-control" value="<?= $title; ?>">
         </div>
         <div class="mb-3">
             <label class="form-label">Product Description</label>
-            <textarea class="form-control" name="description"></textarea>
+            <textarea class="form-control" name="description"><?= $description; ?></textarea>
         </div>
         <div class="mb-3">
             <label class="form-label">Product Price</label>
-            <input type="number" step="0.01" name="price" class="form-control">
+            <input type="number" step="0.01" name="price" class="form-control" value="<?= $price; ?>">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
